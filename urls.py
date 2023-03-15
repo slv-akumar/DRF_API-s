f0a507d64e849233f9fde23ebaffe549a63bcdc4
slv-akumar <121931126+slv-akumar@users.noreply.github.com>
2023-03-15 08:55:00 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,24 @@
+"""djangoauthapi URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 
-from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView, UserListCreateView, UserDetailView, SchoolListCreateView, SchoolDetailView, ClassListCreateView, ClassDetailView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/users/', UserListCreateView.as_view(), name='user_list_create'),
-    path('api/users/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
-    path('api/schools/', SchoolListCreateView.as_view(), name='school_list_create'),
-    path('api/schools/<int:pk>/', SchoolDetailView.as_view(), name='school_detail'),
-    path('api/classes/', ClassListCreateView.as_view(), name='class_list_create'),
-    path('api/classes/<int:pk>/', ClassDetailView.as_view(), name='class_detail'),
+    path('admin/', admin.site.urls),
+    path('api/', include('account.urls')),
 ]
-
 
